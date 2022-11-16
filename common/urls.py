c9bf71c5e03a1d72830ecf9346008f3117439179
norbek2101 <lightnight1223@gmail>
2022-11-16 12:49:40 +0500

    feat: add ListSponsorsView url

--- common/urls.py
+++ common/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from common.views import base_view
 
-
-urlpatterns = []
+urlpatterns = [
+    path("register/", base_view.RegisterSponsorView.as_view(), name="register_sponsor"),
+    path("sponsors/", base_view.ListSponsorsView.as_view(), name="sponsors"),
+]
