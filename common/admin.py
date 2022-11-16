9100b72897d1d4a54408c28a9b704b33249cb671
norbek2101 <lightnight1223@gmail>
2022-11-16 09:15:17 +0500

    feat: Sponsor, Student, University, Sponsorship models was added

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from common.models import Sponsor, Student, University, Sponsorship
 
-# Register your models here.
+class SponsorAdmin(admin.ModelAdmin):
+    list_display = ['user', 'id', 'phone_number',  'balance', 'spent_amount', 'organization', 'type', 'status']
+    list_editable = ['phone_number', 'type', 'status']
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'phone_number',  'balance', 'contract', 'university', 'type')
+    list_editable = ('full_name', 'phone_number', 'type', 'university')
+
+
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_editable = ['name']
+
+
+class SponsorshipAdmin(admin.ModelAdmin):
+    list_display = ['id', 'sponsor', 'student', 'amount']
+
+
+admin.site.register(Sponsor, SponsorAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(University, UniversityAdmin)
+admin.site.register(Sponsorship, SponsorshipAdmin)
--- common/models.py
+++ common/models.py
@@ -1,3 +1,109 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, null=True, verbose_name=_("Дата создания"),)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Дата изменение"),)
+
+    class Meta:
+        abstract = True
+
+
+JISMONIY_SHAXS = "Физическое-лицо"
+YURIDIK_SHAXS = "Юридическое-лицо"
+
+YANGI = "новый"
+MODERATSIYADA = "умеренно"
+TASDIQLANGAN = "подтвержденный"
+BEKOR_QILINGAN = "отмененный"
+
+
+SPONSOR_TYPE = (
+    (JISMONIY_SHAXS, 'jismoniy_shaxs'),
+    (YURIDIK_SHAXS, 'yuridik_shaxs'),
+)
+
+SPONSOR_STATUS = (
+    (YANGI, "yangi"),
+    (MODERATSIYADA, "moderatsiyada"),
+    (TASDIQLANGAN, "tasdiqlangan"),
+    (BEKOR_QILINGAN, "bekor_qilingan"),
+)
+
+class Sponsor(BaseModel):
+    user = models.OneToOneField(User, on_delete = models.CASCADE, related_name="sponsor")
+
+    phone_number = models.CharField(max_length=128)
+    balance = models.DecimalField(max_digits=19, decimal_places=2, default=0)
+    spent_amount = models.DecimalField(max_digits=19, decimal_places=2, default=0)
+    organization = models.CharField(max_length=128, null=True, blank=True)
+
+    type = models.CharField(max_length=50, choices=SPONSOR_TYPE, default=SPONSOR_TYPE[1][1])
+    status = models.CharField(max_length=50, choices=SPONSOR_STATUS, default=SPONSOR_STATUS[0][1])
+   
+    def __str__(self):
+        return f"{self.user.full_name}"
+
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = _("Спонсор")
+        verbose_name_plural = _("Спонсоры")
+
+
+class University(BaseModel):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = _("Университет")
+        verbose_name_plural = _("Университеты")
+
+
+BAKALAVR = "бакалавр"
+MAGISTR = "магистр"
+
+
+STUDENT_TYPE = (
+    (BAKALAVR, "bakalavr"),
+    (MAGISTR, "magistr"),
+)
+
+class Student(BaseModel):
+    full_name = models.CharField(max_length=256)
+    phone_number = models.CharField(max_length=256)
+
+    balance = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    contract = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+
+    type = models.CharField(max_length=50, choices=STUDENT_TYPE)
+
+    university = models.ForeignKey(University, on_delete=models.CASCADE, related_name='students')
+
+    def __str__(self):
+        return self.full_name
+
+
+    class Meta:
+        ordering = ("-id",)
+        verbose_name = _("Студент")
+        verbose_name_plural = _("СтудентЫ")
+
+
+class Sponsorship(BaseModel):
+    sponsor = models.ForeignKey(Sponsor, on_delete=models.CASCADE, related_name='sponsors')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='students')
+    amount = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"Student {self.student} was sponsored by {self.sponsor}"
+
+    class Meta:
+        ordering = ("-id",)
+        verbose_name = _("Спонсорство")
+        verbose_name_plural = _("Спонсорство")
