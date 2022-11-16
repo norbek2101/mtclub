624e23c5408f009ef6b34e9582398300326b4872
norbek2101 <lightnight1223@gmail>
2022-11-16 22:46:23 +0500

    feat: add ListStudentsView, DetailStudentView, UpdateStudentView

--- common/urls.py
+++ common/urls.py
@@ -5,8 +5,12 @@ urlpatterns = [
     path("register/", base_view.RegisterSponsorView.as_view(), name="register_sponsor"),
     path('login/', include('dj_rest_auth.urls')),
     path("sponsors/", base_view.ListSponsorsView.as_view(), name="sponsors"),
-    path('detail/<int:id>/', base_view.DetailSponsorView.as_view(), name="detail_sponsor"),
+    path('sponsors/detail/<int:id>/', base_view.DetailSponsorView.as_view(), name="detail_sponsor"),
 
     path("university/create/", base_view.CreateUniversityView.as_view(), name="create_university"),
 
+    path("student/register/", base_view.RegisterStudentView.as_view(), name="register_student"),
+    path("students/", base_view.ListStudentsView.as_view(), name="students"),
+    path('student/<int:id>/', base_view.DetailStudentView.as_view(), name="detail_student"),
+    path('student/update/<int:id>/', base_view.UpdateStudentView.as_view(), name="update_student"),
 ]
--- common/views/base_view.py
+++ common/views/base_view.py
@@ -10,7 +10,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 from core.custom_pagination import CustomPagination
 
-class RegisterSponsorView(generics.ListCreateAPIView):
+class RegisterSponsorView(generics.CreateAPIView):
     serializer_class = RegisterSponsorSerializer
     permission_classes = [permissions.IsAuthenticated]
     parser_classes = [parsers.MultiPartParser]
@@ -51,3 +51,48 @@ class CreateUniversityView(generics.ListCreateAPIView):
     pagination_class = CustomPagination
 
     permission_classes = [permissions.IsAuthenticated]
+
+
+class RegisterStudentView(generics.CreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = RegisterStudentSerializer
+    pagination_class = CustomPagination
+    parser_classes = [parsers.MultiPartParser]
+
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ListStudentsView(generics.ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = ListStudentsSerializer
+    pagination_class = CustomPagination
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('type', 'university')
+    search_fields = ('name',)
+
+
+class DetailStudentView(generics.RetrieveDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = DetailStudentSerializer
+    pagination_class = CustomPagination
+    parser_classes = [parsers.MultiPartParser]
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.kwargs.get('id', None):
+            queryset = queryset.filter(id=self.kwargs['id'])
+
+        return queryset
+
+
+class UpdateStudentView(generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = UpdateStudentSerializer
+    pagination_class = CustomPagination
+    parser_classes = [parsers.MultiPartParser]
+    lookup_field = 'id'
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
