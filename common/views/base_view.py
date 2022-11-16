bbf16cf36ffa6274e247a2007df1baf377e8270c
norbek2101 <lightnight1223@gmail>
2022-11-16 12:49:19 +0500

    feat: add ListSponsorsView

--- common/views/base_view.py
+++ common/views/base_view.py
@@ -5,5 +5,26 @@ from common.serializers.base_serializers import RegisterSponsorSerializer, ListS
     DetailStudentSerializer, UpdateStudentSerializer, SponsorshipSerializer, UpdateSponsorshipSerializer, \
     LineDashboardSponsorsSerializer, LineDashboardStudentsSerializer
 
+from rest_framework import generics, permissions, parsers
+from django_filters.rest_framework import DjangoFilterBackend
+
+from core.custom_pagination import CustomPagination
+
+class RegisterSponsorView(generics.ListCreateAPIView):
+    serializer_class = RegisterSponsorSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    parser_classes = [parsers.MultiPartParser]
+    queryset = Sponsor.objects.all()
+    pagination_class = CustomPagination
+
+
+class ListSponsorsView(generics.ListAPIView):
+    queryset = Sponsor.objects.all()
+    serializer_class = ListSponsorsSerializer
+    pagination_class = CustomPagination
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('status', 'balance')
+    search_fields = ('name',)
 
 
