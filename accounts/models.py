51e79d2e69cb73eb3ba01926ec00f120f2dbf9ff
norbek2101 <lightnight1223@gmail>
2022-11-16 05:28:33 +0500

    add User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    full_name = models.CharField(max_length=256, verbose_name=_("ФИО"),)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, verbose_name=_("Дата создания"),)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Дата изменение"),)
+
+    class Meta:
+        db_table = "user"
+        swappable = "AUTH_USER_MODEL"
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
